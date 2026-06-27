e64cc3a21e49e54ebfae0caa333ef37dd974c309
liweimin39 <951201794@qq.com>
2026-06-28 04:30:25 +0800

    Delete cloud-functions directory

--- cloud-functions/api/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# 使 utils 成为一个 Python 包
-from .client import JmClient
-from .parser import JmParser
-from .models import JmAlbum, JmPhoto, JmImage
-from .crypto import JmCrypto
-
-__all__ = ['JmClient', 'JmParser', 'JmAlbum', 'JmPhoto', 'JmImage', 'JmCrypto']
